51086ccc0932a2a8ea32c06b3df2bfd1f8625073
johblack18 <34717560+johblack18@users.noreply.github.com>
2024-10-26 15:57:47 +0200

    Update bms.py

--- pace-bms-dev/bms.py
+++ pace-bms-dev/bms.py
@@ -53,9 +53,13 @@ temps = 6
 
 print("Connection Type: " + connection_type)
 
-def on_connect(client, userdata, flags, reason_code):
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
     client.subscribe("$SYS/#")
+
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
@@ -64,25 +68,45 @@ mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 
+mqttc.connect("10.0.0.64", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+mqttc.loop_forever()
+
+
+#def on_connect(client, userdata, flags, reason_code):
+#    print(f"Connected with result code {reason_code}")
+#    client.subscribe("$SYS/#")
+# The callback for when a PUBLISH message is received from the server.
+#def on_message(client, userdata, msg):
+#    print(msg.topic+" "+str(msg.payload))
+
+#mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+#mqttc.on_connect = on_connect
+#mqttc.on_message = on_message
+
     
-    client.will_set(config['mqtt_base_topic'] + "/availability","offline", qos=0, retain=False)
-    global mqtt_connected
-    mqtt_connected = True
+#    client.will_set(config['mqtt_base_topic'] + "/availability","offline", qos=0, retain=False)
+#    global mqtt_connected
+#    mqtt_connected = True
 
-def on_disconnect(client, userdata, rc):
-    print("MQTT disconnected with result code "+str(rc))
-    global mqtt_connected
-    mqtt_connected = False
+#def on_disconnect(client, userdata, rc):
+#    print("MQTT disconnected with result code "+str(rc))
+#    global mqtt_connected
+#    mqtt_connected = False
 
 
-client = mqtt.Client("bmspace")
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
+#client = mqtt.Client("bmspace")
+#client.on_connect = on_connect
+#client.on_disconnect = on_disconnect
 #client.on_message = on_message
 
-client.username_pw_set(username=config['mqtt_user'], password=config['mqtt_password'])
-client.connect(config['mqtt_host'], config['mqtt_port'], 60)
-client.loop_start()
+#client.username_pw_set(username=config['mqtt_user'], password=config['mqtt_password'])
+#client.connect(config['mqtt_host'], config['mqtt_port'], 60)
+#client.loop_start()
 time.sleep(2)
 
 def exit_handler():
